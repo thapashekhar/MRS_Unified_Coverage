52fb148133ae6c070d959ec24327fcad39d1c560
swaraj222 <swaraj.nistane@gmail.com>
2025-03-12 00:06:57 -0400

    Update unified_coverage1_new.py

--- robotarium_python_simulator_MRS_UGA/rps/mrs_unified_coverage/unified_coverage1_new.py
+++ robotarium_python_simulator_MRS_UGA/rps/mrs_unified_coverage/unified_coverage1_new.py
@@ -4,15 +4,14 @@ from rps.utilities.transformations import *
 from rps.utilities.barrier_certificates import *
 from rps.utilities.misc import *
 from rps.utilities.controllers import *
-
+from scipy.spatial import ConvexHull
 import numpy as np
 
 # Instantiate Robotarium object
 N = 5
 
 
-L = completeGL(N)  # Connectivity of robots (complete graph)
-iterations = 1000  # Number of steps/iteration to run the simulation
+# L = completeGL(N)  # Connectivity of robots (complete graph)
 
 # Sensor type
 S = [1, 2]
@@ -47,7 +46,7 @@ weight_i = np.zeros(N)
 # weight_i[0] = 1
 weight_i = np.array([1, 2, 1, 1, 4, 1, 1, 1, 1, 1])
 
-r = robotarium.Robotarium(number_of_robots=N, show_figure=True, sim_in_real_time=True)
+robo = robotarium.Robotarium(number_of_robots=N, show_figure=True, sim_in_real_time=False, initial_conditions=initial_conditions[0:N].T)
 
 # How many iterations do we want (about N*0.033 seconds)
 iterations = 500
@@ -59,8 +58,24 @@ si_barrier_cert = create_single_integrator_barrier_certificate_with_boundary()
 # Create SI to UNI dynamics tranformation
 si_to_uni_dyn, uni_to_si_states = create_si_to_uni_mapping()
 
+safety_radius = 0.1
 # Generated a connected graph Laplacian (for a cylce graph).
 L = lineGL(N)
+
+CM = [
+    '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', 
+    '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+    '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5',
+    '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5'
+    ]
+
+safety_radius_marker_size = determine_marker_size(robo, safety_radius)  # Will scale the plotted markers to be the diameter of provided argument (in meters)
+font_height_meters = 0.1
+font_height_points = determine_font_size(robo, font_height_meters)  # Will scale the plotted font height to that of the provided argument (in meters)
+marker_size_goal = determine_marker_size(robo, 0.2)
+font_size_m = 0.1
+font_size = determine_font_size(robo, font_size_m)
+line_width = 5
 
 x_min = -1.5
 x_max = 1.5
@@ -68,34 +83,81 @@ y_min = -1
 y_max = 1
 res = 0.05
 
- 
+# Plot workspace boundary
+boundary_points = [[x_min, y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min], [x_min, y_min]]
+bound_x, bound_y = zip(*boundary_points)
+square, = robo.axes.plot(bound_x, bound_y, 'b-', linewidth=5)
+robo.axes.set_xlim(x_min - 0.2, x_max + 0.2)
+robo.axes.set_ylim(y_min - 0.2, y_max + 0.2)
+
+# Initial plots
+x_i = robo.get_poses()
+g = robo.axes.scatter(x_i[0, :], x_i[1, :], s=np.pi / 4 * safety_radius_marker_size, marker='o', facecolors='none', edgecolors=CM, linewidth=3)
+
+# Robot number text generator
+robot_number_text = np.empty((3, 0))
+for jj in range(1, N + 1):
+    robot_number_text = np.append(robot_number_text, '{0}'.format(jj))
+
+# Initial labeling of robots
+robot_labels = [robo.axes.text(x_i[0, kk], x_i[1, kk] + 0.2, robot_number_text[kk], fontsize=font_size, color='b', fontweight='bold', horizontalalignment='center', verticalalignment='center', zorder=0)
+                for kk in range(0, N)]
+
+robo.step()  # Iterate the simulation
+x_global_values = np.arange(x_min, x_max + res, res)
+y_global_values = np.arange(y_min, y_max + res, res)
+hull_local_handler = []
+locations = [[] for _ in range(N)]
 
 for k in range(iterations):
     print('iteration k = ', k)
     # Get the poses of the robots and convert to single-integrator poses
-    x = r.get_poses()
+    x = robo.get_poses()
     x_si = uni_to_si_states(x)
     current_x = x_si[0,:,None]
     current_y = x_si[1,:,None]
-  
+    
+    for q in range(N):
+        robot_labels[q].set_position([x_si[0, q], x_si[1, q] + 0.2])
+        robot_labels[q].set_fontsize(determine_font_size(robo, font_size_m))
+
+    # Update Plotted Visualization
+    g.set_offsets(x[:2, :].T)
+    # This updates the marker sizes if the figure window size is changed.
+    # This should be removed when submitting to the Robotarium.
+    g.set_sizes([determine_marker_size(robo, safety_radius)])
+
     c_v = np.zeros((N,2))
     w_v = np.zeros(N)
     weigth = np.zeros(N)
-    weigth = np.array([1, 2, 1, 1, 4])
+    weigth = 0.1 * np.array([1, 1, 1, 4, 4])
     for ix in np.arange(x_min,x_max,res):
         for iy in np.arange(y_min,y_max,res):
             importance_value = 1
             distances = np.zeros(N)
             for robots in range(N):
                 distances[robots] = np.sqrt(np.square(ix - current_x[robots]) + np.square(iy - current_y[robots])- weigth[robots])
+            # print("distances", distances)
             min_index = np.argmin(distances)
             c_v[min_index][0] += ix * importance_value
             c_v[min_index][1] += iy * importance_value
             w_v[min_index] += 1
+            locations[min_index].append([ix, iy])
    
          
                 
-                
+    if k>0:
+        [plot.remove() for plot in hull_figHandles] 
+    hull_figHandles =[]
+    for r in range(N):
+        q_points = np.array(locations[r])
+        hull = ConvexHull(q_points)
+        boundary_points = q_points[hull.vertices]
+        xss, yss = boundary_points[:, 0], boundary_points[:, 1]
+        xss = np.concatenate((xss, [xss[0]]))   # hull.vertices does not provide closed boundary, adding a clyclic vertices for enclosed geometry
+        yss = np.concatenate((yss, [yss[0]]))
+        hullHandle, =  (robo.axes.plot(xss, yss,'b-',linewidth =8))
+        hull_figHandles.append(hullHandle)  
     # Initialize the single-integrator control inputs
     si_velocities = np.zeros((2, N))
     
@@ -118,9 +180,9 @@ for k in range(iterations):
     dxu = si_to_uni_dyn(si_velocities, x)
 
     # Set the velocities of agents 1,...,N
-    r.set_velocities(np.arange(N), dxu)
+    robo.set_velocities(np.arange(N), dxu)
     # Iterate the simulation
-    r.step()
+    robo.step()
 
 #Call at end of script to print debug information and for your script to run on the Robotarium server properly
-r.call_at_scripts_end()
+robo.call_at_scripts_end()
